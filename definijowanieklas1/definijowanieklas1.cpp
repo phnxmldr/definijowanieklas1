4b57b57530916c6cc582e7dc01bda19b0a9fc70d
3cg1 <3cg1@S30214>
2021-10-06 13:38:32 +0200

    Dodaj pliki projektów.

---
+++ definijowanieklas1/definijowanieklas1.cpp
@@ -0,0 +1,42 @@
+﻿// definijowanieklas1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std; 
+
+class Triangle {
+public:
+	int bok_a;
+	int bok_b;
+	int bok_c;
+	int h;
+
+	void obliczPole() {
+		cout << "Pole = " << bok_a + bok_b + bok_c << endl;
+	}
+	void obliczObwod() {
+		cout << "Obwod = " << (bok_a * h) / 2 << endl	;
+	}
+};
+
+int main() {
+	Triangle triangle;
+	int x;
+	int y;
+	int z;
+	int a;
+	cout << "Podaj bok a - ";
+	cin >> x;
+	triangle.bok_a = x;
+	cout << "Podaj bok b - ";
+	cin >> y;
+	triangle.bok_b = y;
+	cout << "Podaj bok c - ";
+	cin >> z;
+	triangle.bok_c = z;
+	cout << "Podaj h - ";
+	cin >> a;
+	triangle.h = a;
+	triangle.obliczPole();
+	triangle.obliczObwod();
+};
